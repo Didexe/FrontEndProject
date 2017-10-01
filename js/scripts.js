8442ec6d5cd5944bddc681dfca52304aa25bd06d
Daniel <didexe@gmail.com>
2017-10-01 18:17:42 +0300

    home page styles finished. post page added

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const SLIDERIMAGES = []
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  getSliderImages();
+} else {
+  document.addEventListener("DOMContentLoaded", callback);
+}
+
+function getSliderImages() {
+    
+}
+function changeSliderImages() {
+
+}
